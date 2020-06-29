e8cfa8ce8d0b2bdb7ef789002e4ff4f8b0c1e490
thundermiracle <cissoliu@gmail.com>
2020-06-29 17:11:53 +0900

    import webpack

---
+++ README.md
@@ -0,0 +1,23 @@
+# css-animations
+
+Use css only to animate site.
+
+## Ideas
+
+Come from the course in [https://cssanimation.rocks/](https://cssanimation.rocks/) with Webpack HMR supported.
+
+## Development
+
+```shell
+yarn
+yarn dev
+```
+
+## Production
+
+```shell
+yarn
+yarn build
+```
+
+Output files to `./dist` folder.
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+// Common Plugins
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackMerge = require('webpack-merge');
+
+// Development Plugins
+const Webpack = require('webpack');
+
+// Production Plugins
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+// production or development
+const devMode = !process.argv.includes('--mode=production');
+
+const commonConfig = {
+  entry: path.resolve(__dirname, './src/main.js'),
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: './images/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+// create config by mode
+const extraConfig = devMode
+  ? {
+      mode: 'development',
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        port: 3000,
+        hot: true,
+        contentBase: './dist',
+      },
+      plugins: [new Webpack.HotModuleReplacementPlugin()],
+    }
+  : {
+      mode: 'production',
+      devtool: 'cheap-module-source-map',
+      optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      },
+    };
+
+module.exports = WebpackMerge(commonConfig, extraConfig);
