ead948195daf306c027c8d0720398efc7603992e
thundermiracle <cissoliu@gmail.com>
2020-07-05 15:50:53 +0900

    finish the hero header

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 import './css/main.css';
 import './css/header.css';
+
+document.body.className += ' js-loading';
+
+window.addEventListener('load', showPage, false);
+
+function showPage() {
+  document.body.className = document.body.className.replace('js-loading', '');
+}
