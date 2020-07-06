885987241713d6ba3d8bce807841f32956af2b03
thundermiracle <cissoliu@gmail.com>
2020-07-06 21:12:04 +0900

    add tooltip part

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './css/main.css';
 import './css/header.css';
+import './css/link.css';
 
+/**
+ * JS for header
+ */
 document.body.className += ' js-loading';
 
 window.addEventListener('load', showPage, false);
@@ -8,3 +12,16 @@ window.addEventListener('load', showPage, false);
 function showPage() {
   document.body.className = document.body.className.replace('js-loading', '');
 }
+
+/**
+ * JS for tooltips
+ */
+const anchors = document.querySelectorAll('.anchor-tooltip');
+anchors.forEach((anchor) => {
+  const tooltipText = anchor.getAttribute('title');
+
+  const tooltip = document.createElement('span');
+  tooltip.className = 'title-tooltip';
+  tooltip.innerHTML = tooltipText;
+  anchor.append(tooltip);
+});
